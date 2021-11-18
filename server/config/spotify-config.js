77e9ed92676305f2fb4104bc3d9d0782cad548be
srivennela2019 <49042098+srivennela2019@users.noreply.github.com>
2021-11-18 18:29:09 -0500

    Removed client id and secret

--- server/config/spotify-config.js
+++ server/config/spotify-config.js
@@ -1,6 +1,6 @@
 const qs = require('qs');
-var client_id = '8afb75536efd46a89de783da23889875'; // Your client id
-var client_secret = 'afbb158b653a40dcb3fbe96ffb809a0a'; // Your secret
+var client_id = 'client id'; // Your client id
+var client_secret = 'client secret'; // Your secret
 const axios = require('axios');
 
  getAuth = async () => {
